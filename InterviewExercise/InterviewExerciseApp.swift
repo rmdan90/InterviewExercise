967725814ecd713f684ca7aade77f5beabab3937
Ramadan Alharahsheh <rmdan90@gmail.com>
2025-02-03 21:14:40 +0300

    Initial Commit

---
+++ InterviewExercise/InterviewExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterviewExerciseApp.swift
+//  InterviewExercise
+//
+//  Created by ramadan Al on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct InterviewExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InterviewExerciseUITests/InterviewExerciseUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  InterviewExerciseUITestsLaunchTests.swift
+//  InterviewExerciseUITests
+//
+//  Created by ramadan Al on 2/3/25.
+//
+
+import XCTest
+
+final class InterviewExerciseUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
